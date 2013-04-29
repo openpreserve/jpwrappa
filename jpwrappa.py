a20969e196d764bdbb200170d7aadf36c2040767
Johan van der Knijff <johan.vanderknijff@kb.nl>
2013-04-29 17:11:09 +0200

    bumped minor version number

--- jpwrappa.py
+++ jpwrappa.py
@@ -32,7 +32,7 @@ import etpatch as ET
 import shared
 scriptPath, scriptName = os.path.split(sys.argv[0])
 
-__version__= "0.1.5"
+__version__= "0.1.6"
 
 def main_is_frozen():
     return (hasattr(sys, "frozen") or # new py2exe
