fe5dbd04a9fff2c4fe2a84b919528b0e6cd58f6b
Johan van der Knijff <johan.vanderknijff@kb.nl>
2013-05-01 15:32:59 +0200

    rearranged directory structure, added minimalistic setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='jpwrappa',
+    version='0.1.6',
+    packages=['jpwrappa',],
+    license='Apache License, Version 2.0',
+    long_description=open('README.md').read(),
+)
