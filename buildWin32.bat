b68c53b7edae2896a5c74c0c7066310a1a0e1d29
Johan van der Knijff <johan.vanderknijff@kb.nl>
2013-05-06 18:22:59 +0200

    build process for win32 now works

---
+++ buildWin32.bat
@@ -0,0 +1,66 @@
+:
+:: Build 32 bit Windows jpylyzer binaries from Python script, and pack them in ZIP file
+::
+:: ZIP file includes license file, PDF User Manual and example files 
+::
+:: Johan van der Knijff, 25 april 2013
+::
+:: Dependencies:
+:: 
+:: - Python 2.7  (PyInstaller doesn't work with Python 3 yet!) 
+:: - PyInstaller 2: http://www.pyinstaller.org/
+:: - PyWin32 (needed by PyInstaller): http://sourceforge.net/projects/pywin32/files/
+:: - a spec file with 
+::
+@echo off
+setlocal
+
+::::::::: CONFIGURATION :::::::::: 
+
+:: Python
+set python=c:\python27\python
+
+:: Path to PyInstaller
+set pathPyInstaller=c:\pyinstall\
+
+:: Script base name (i.e. script name minus .py extension)
+set scriptBaseName=jpwrappa
+
+:: PyInstaller spec file that defines build options
+set specFile=jpwrappa_win32.spec
+
+:: Directory where build is created (should be identical to 'name' in 'coll' in spec file!!)
+set distDir=.\dist_win32\
+
+:: Executes jpylyzer with -v option and stores output to 
+:: env variable 'version'
+set vCommand=%python% .\%scriptBaseName%\%scriptBaseName%.py -v
+%vCommand% 2> temp.txt
+set /p version= < temp.txt
+del temp.txt 
+
+::::::::: BUILD :::::::::::::::::: 
+
+:: Build binaries
+%python% %pathPyInstaller%\pyinstaller.py %specFile%
+
+:: Generate name for ZIP file
+set zipName=%scriptBaseName%_%version%_win32.zip
+
+:: Create ZIP file
+%python% zipdir.py %distDir%\%scriptBaseName% %distDir%\%zipName% 
+
+::::::::: CLEANUP ::::::::::::::::: 
+
+:: Delete build directory
+rmdir build /S /Q
+
+:: Delete jpylyzer directory in distdir
+rmdir %distDir%\%scriptBaseName% /S /Q
+
+::::::::: PARTY TIME! ::::::::::::::::: 
+
+echo /
+echo Done! Created %zipName% in directory %distDir%!
+echo / 
+
---
+++ jpwrappa_win32.spec
@@ -0,0 +1,32 @@
+# -*- mode: python -*-
+a = Analysis(['.\jpwrappa\jpwrappa.py'],
+             pathex=['.\jpwrappa'],
+             hiddenimports=[],
+             hookspath=None)
+pyz = PYZ(a.pure)
+exe = EXE(pyz,
+          a.scripts,
+          exclude_binaries=1,
+          name=os.path.join('build\\pyi.win32\\jpwrappa', 'jpwrappa.exe'),
+          debug=False,
+          strip=None,
+          upx=True,
+          console=True )
+coll = COLLECT(exe,
+               a.binaries +
+               [('./license/LICENSE.txt','LICENSE','DATA')],
+               [('./doc/jpWrappaUserManual.html','./doc/jpWrappaUserManual.html','DATA')],
+               [('./config.xml','./jpwrappa/configWin.xml','DATA')],
+               [('./profiles/default.xml','./jpwrappa/profiles/default.xml','DATA')],
+               [('./profiles/demoAccessLossy.xml','./jpwrappa/profiles/demoAccessLossy.xml','DATA')],
+               [('./profiles/demoLosslessHarvard.xml','./jpwrappa/profiles/demoLosslessHarvard.xml','DATA')],
+               [('./profiles/demoLossyHarvard.xml','./jpwrappa/profiles/demoLossyHarvard.xml','DATA')],
+               [('./profiles/demoLossyJpylyzer.xml','./jpwrappa/profiles/demoLossyJpylyzer.xml','DATA')],
+               [('./profiles/demoMasterLossless.xml','./jpwrappa/profiles/demoMasterLossless.xml','DATA')],
+               [('./profiles/optionsKBAccessLossy.xml','./jpwrappa/profiles/optionsKBAccessLossy.xml','DATA')],
+               [('./profiles/optionsKBMasterLossless.xml','./jpwrappa/profiles/optionsKBMasterLossless.xml','DATA')],     
+               a.zipfiles,
+               a.datas,
+               strip=None,
+               upx=True,
+               name=os.path.join('dist_win32', 'jpwrappa'))
